c696c8637a42494bb5820a0e4ed086f8c24522e3
Akash <akashverma0768@gmail.com>
2024-01-14 16:46:15 +0530

    successfully ran chat testing

--- src/chat/models.py
+++ src/chat/models.py
@@ -4,7 +4,7 @@ from django.db import models
 User = get_user_model()
 # Create your models here.
 class Message(models.Model):
-    author = models.ForeignKey(User, related_name="author_messages", on_delete = models.CASCADE)
+    author = models.ForeignKey(User, related_name="author_messages", on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
